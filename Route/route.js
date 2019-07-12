db8446cd339548b64b05fff285ccc231741b1b2b
Juwar <juwarthunderboy@gmail.com>
2019-07-13 06:04:27 +0700

    all task

--- Route/route.js
+++ Route/route.js
@@ -1,6 +1,6 @@
 'use strict'
 
-module.exports = function(apps){
+module.exports = function (apps) {
     //======================================
     //IMPORT CONTROLLER NAME
     const controler = require('../Controller/productConntroller')
@@ -10,52 +10,59 @@ module.exports = function(apps){
     const transactionController = require('../Controller/transactionController')
     const mailerController = require('../Controller/mailerController')
     const forgetController = require('../Controller/forgetController')
+    const wishlistController = require('../Controller/wishlistController')
+    const mostPopuler = require('../Controller/mostPopuler')
+    const searchAll = require('../Controller/searchAll')
     const auth = require('../Middleware/verifytoken')
     const multer = require('multer')
 
     const storage = multer.diskStorage({
-        destination: function(req, file, callback){
-            callback(null,'./uploads');
+        destination: function (req, file, callback) {
+            callback(null, './uploads');
         },
-        filename: function(req, file, callback){
-            callback(null,file.originalname);
+        filename: function (req, file, callback) {
+            callback(null, file.originalname);
         }
     });
 
-    let upload = multer({storage: storage})
-    const verif = require('../Controller/verifytoken')
-    apps.get('/',controler.hello)
-    apps.get('/product',controler.getProduct)
-    apps.post('/product',upload.single('image'), controler.postImage)
-    apps.delete('/product/:id',controler.deleteProduct)
-    apps.patch('/product/:id',controler.updateProduct)
+    let upload = multer({ storage: storage })
 
-    apps.get('/cart/:id',cartController.getCart)
-    apps.post('/cart',cartController.postCart)
+    apps.get('/', controler.hello)
+    apps.get('/product', controler.getProduct)
+    apps.post('/product', upload.single('image'), controler.postImage)
+    apps.delete('/product/:id', controler.deleteProduct)
+    apps.patch('/product/:id', controler.updateProduct)
+
+    apps.get('/cart/:id', cartController.getCart)
+    apps.post('/cart', cartController.postCart)
     apps.delete('/cart/:id', cartController.deleteCart)
-    apps.patch('/cart/:id',cartController.updateCart)
+    apps.patch('/cart/:id', cartController.updateCart)
 
-    apps.get('/user/:id',auth, userController.getUser)
-    apps.post('/user', upload.single('image'),userController.postUser)
+    apps.get('/user/:id', userController.getUser)
+    apps.post('/user', upload.single('image'), userController.postUser)
 
     apps.delete('/user/:id', userController.deleteUser)
     apps.patch('/user/:id', userController.updateUser)
 
-    apps.get('/transaction/:id',transactionController.getTransaction)
-    apps.post('/transaction',transactionController.postTransaction)
+    apps.get('/transaction', transactionController.getTransaction)
+    apps.post('/transaction', transactionController.postTransaction)
     apps.delete('/transaction', transactionController.deleteTransaction)
     apps.patch('/transaction', transactionController.updateTransaction)
 
-    apps.post('/auth',authController.postAuth)
+    apps.get('/wishlist', wishlistController.getWishlist)
+    apps.post('/wishlist', wishlistController.postWishlist)
+    apps.delete('/wishlist/:id', wishlistController.deleteWishlist)
 
-    apps.patch('/auth/:id',authController.changeAuth)
-    apps.post('/upload',upload.single('image'), controler.postImage)
+    apps.post('/auth', authController.postAuth)
+    apps.patch('/auth/:id', authController.changeAuth)
+    apps.post('/upload', upload.single('image'), controler.postImage)
 
     apps.get('/category', controler.getCategory)
     apps.post('/category', controler.postCategory)
 
-    apps.get('/mail', mailerController)
-    apps.patch('/forget/:id', forgetController.forgetPassword)
+    apps.post('/mail', mailerController)
+    apps.patch('/forget', forgetController.forgetPassword)
 
-
+    apps.patch('/mostPopuler/:id', mostPopuler)
+    apps.get('/search', searchAll)
 }
